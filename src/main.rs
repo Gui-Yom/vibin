810b7562e116d62d0fd57692003b1ce9cbf7aebe
Guillaume Anthouard <guillaume.anthouard@hotmail.fr>
2020-11-26 21:52:19 +0100

    now there's a gui

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,37 @@
-use std::fs::File;
+use std::{fs::File, io::BufReader};
 
-use av_format::buffer::AccReader;
-use matroska::demuxer::MkvDemuxer;
-use rustav::RustAVSource;
+use winit::{dpi::LogicalSize, event_loop::EventLoop, window::WindowBuilder};
 
-mod rustav;
 fn main() {
-    // Open the matroska file
-    let reader =
-        File::open("C:/Users/Guillaume/Desktop/memelord_music_pack/widewalk_mono.webm").unwrap();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Vibin")
+        .with_resizable(false)
+        .with_decorations(false)
+        .with_visible(true)
+        .with_inner_size(LogicalSize::new(128, 128))
+        .build(&event_loop)
+        .unwrap();
 
-    // Create a buffer of size 4096 B to contain matroska data
-    let ar = AccReader::with_capacity(8192, reader);
+    // Open the matroska file
+    let file = File::open("C:/Users/Guillaume/Desktop/memelord_music_pack/widewalk.ogg").unwrap();
 
     // Ignore unused variable warning, stream should not be dropped
     let (stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
     let sink = rodio::Sink::try_new(&stream_handle).unwrap();
 
     // Add a dummy source of the sake of the example.
-    let source = RustAVSource::new(Box::new(MkvDemuxer::new()), Box::new(ar))
-        .expect("Can't create OpusDecoder");
+    let source = rodio::Decoder::new_vorbis(BufReader::new(file)).unwrap();
 
     sink.append(source);
     //sink.append(rodio::source::SineWave::new(440));
     sink.set_volume(0.6);
 
-    // Stop with ctrl+c
-    loop {
-        std::thread::sleep(std::time::Duration::from_millis(1000));
-    }
+    event_loop.run(|e, target, control_flow| match e {
+        winit::event::Event::WindowEvent { window_id, event } => match event {
+            WindowEvent => {}
+            _ => {}
+        },
+        _ => {}
+    });
 }
