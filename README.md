2964f84a1392a2a5a3fdb30ded038ff1a761f512
Guillaume Anthouard <guillaume.anthouard@hotmail.fr>
2023-05-04 22:09:07 +0200

    Now use winit, softbuffer and mutself

--- README.md
+++ README.md
@@ -1,10 +1,44 @@
 # Vibin
 
-For when your last 2 brain cells need to vibe together at 4am (blazingly fast).
+For when your last 2 brain cells need to vibe together at 4am.
 
-![cat vibing](cat.gif)
+![cat vibing](gifs/cat.gif)
+![polish cow](gifs/polish_cow.gif)
+![dance](gifs/dance.gif)
+
+![future rocks](gifs/mirai.gif)
 
 ---
+
+## Usage
+
+1. Take a deep breath
+2. Choose your favorite gif and audio. Recommendations :
+    * Gif that loops : [example collection](https://github.com/Gui-Yom/vibin/tree/master/gifs)
+    * Audio that destroys your mind, something really catchy. The goal here is for you to attain flow state rapidly.
+      Non-exhaustive list :
+        * https://youtu.be/Z07z0giGG-w?t=45
+        * https://youtu.be/230NWoF-vS4
+        * https://youtu.be/9hhMUT2U2L4
+        * https://youtu.be/stdyhfeYxxI
+        * https://youtu.be/NUYvbT6vTPs
+        * https://youtu.be/w0AOGeqOnFY
+        * Any Drum & Bass song like https://on.soundcloud.com/BqG4R
+3. Obtain a **vibin** executable through the means of [compilation](#build--run)
+   or [downloading](https://github.com/Gui-Yom/vibin/releases).
+4. Drag your music and your audio over the vibin executable.
+5. Your new customized vibin is served hot and fresh
+6. Continue doing whatever you were doing
+7. ???
+8. Profit
+9. *Bonus points* if people judge you for using this dark method.
+
+## Controls
+
+- The window is freely movable by dragging it.
+- Left click to pause audio
+- Mouse wheel to adjust volume
+- Middle mouse button to close it
 
 ## Build & Run
 
@@ -30,15 +64,6 @@ cargo build --release --no-default-features --features bundle-audio
 target/release/vibin
 ```
 
-## Controls
+---
 
-- The window is freely movable by dragging it.
-- Right-click to play next item
-- Mouse wheel to adjust volume
-- Middle mouse button to close it
-
-https://www.reddit.com/r/ProgrammerHumor/comments/jtnrlk/everyone_loves_pointers_right/?utm_source=share&utm_medium=web2x&context=3
-
-## A note about build optimization
-
-See [BuildOpt.md](BuildOpt.md).
+https://www.reddit.com/r/ProgrammerHumor/comments/jtnrlk/everyone_loves_pointers_right/
--- src/main.rs
+++ src/main.rs
@@ -2,131 +2,22 @@
 
 use std::io::Cursor;
 use std::time::{Duration, Instant};
-use std::{env, fs};
+use std::{env, fs, slice};
 
-use eframe::epaint::Rgba;
-use egui::{ColorImage, Pos2, TextureHandle, TextureOptions, Vec2, Visuals};
 use image::codecs::gif::GifDecoder;
-use image::AnimationDecoder;
+use image::{AnimationDecoder, ImageDecoder};
 use kira::manager::backend::DefaultBackend;
 use kira::manager::{AudioManager, AudioManagerSettings, MainPlaybackState};
 use kira::sound::static_sound::{StaticSoundData, StaticSoundSettings};
 use kira::tween::{Easing, Tween};
 use kira::{LoopBehavior, StartTime};
+use softbuffer::GraphicsContext;
+use winit::dpi::{LogicalSize, PhysicalPosition};
+use winit::event::{ElementState, Event, MouseButton, MouseScrollDelta, WindowEvent};
+use winit::event_loop::EventLoop;
+use winit::window::{WindowBuilder, WindowLevel};
 
 mod staticdata;
-
-pub struct Vibin {
-    pos: Pos2,
-    images: Vec<(TextureHandle, u32)>,
-    current: usize,
-    last_change: Instant,
-    audio: AudioManager,
-    volume: f64,
-}
-
-impl Vibin {
-    pub fn new(cc: &eframe::CreationContext<'_>, images: Vec<(TextureHandle, u32)>) -> Self {
-        cc.egui_ctx.set_visuals(Visuals::dark());
-
-        let mut manager = AudioManager::<DefaultBackend>::new(AudioManagerSettings::default())
-            .expect("Can't initialize audio context");
-
-        let settings = StaticSoundSettings::new()
-            .volume(0.8)
-            .loop_behavior(LoopBehavior {
-                start_position: 0.0,
-            });
-
-        let audio =
-            StaticSoundData::from_cursor(Cursor::new(staticdata::get_audio_data()), settings)
-                .expect("Can't decode bundled sound file");
-
-        manager.play(audio).expect("Can't play sound");
-
-        Self {
-            pos: Pos2::new(100.0, 100.0),
-            images,
-            current: 0,
-            last_change: Instant::now(),
-            audio: manager,
-            volume: 1.0,
-        }
-    }
-}
-
-impl eframe::App for Vibin {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        {
-            let input = ctx.input();
-            if input.pointer.middle_down() {
-                frame.close();
-            }
-
-            if input.pointer.primary_down() {
-                if let Some(orig) = input.pointer.press_origin() {
-                    if let Some(end) = input.pointer.interact_pos() {
-                        self.pos += end - orig;
-                        frame.set_window_pos(self.pos);
-                    }
-                }
-            }
-
-            if input.pointer.secondary_down() && input.pointer.any_pressed() {
-                match self.audio.state() {
-                    MainPlaybackState::Paused | MainPlaybackState::Pausing => self
-                        .audio
-                        .resume(Tween {
-                            start_time: StartTime::Immediate,
-                            duration: Duration::from_millis(350),
-                            easing: Easing::InPowf(1.0),
-                        })
-                        .expect("Can't resume audio"),
-                    MainPlaybackState::Playing => self
-                        .audio
-                        .pause(Tween {
-                            start_time: StartTime::Immediate,
-                            duration: Duration::from_millis(350),
-                            easing: Easing::OutPowf(1.0),
-                        })
-                        .expect("Can't pause audio"),
-                }
-            }
-
-            self.volume = (self.volume + input.scroll_delta.y as f64 / 1000.0).clamp(0.0, 1.0);
-            self.audio
-                .main_track()
-                .set_volume(self.volume, Tween::default())
-                .expect("Can't set volume");
-        }
-
-        let delta =
-            self.images[self.current].1 as i32 - self.last_change.elapsed().as_micros() as i32;
-        if delta <= 0 {
-            self.current = (self.current + 1) % self.images.len();
-            self.last_change = Instant::now();
-            ctx.request_repaint();
-        } else {
-            ctx.request_repaint_after(Duration::from_micros(delta as u64));
-        }
-
-        egui::Area::new("main_area")
-            .fixed_pos(Pos2::ZERO)
-            .show(ctx, |ui| {
-                ui.image(&self.images[self.current].0, Vec2::new(SIZE, SIZE))
-            });
-    }
-
-    /// Called by the framework to save state before shutdown.
-    fn save(&mut self, _storage: &mut dyn eframe::Storage) {}
-
-    fn clear_color(&self, _visuals: &Visuals) -> Rgba {
-        Rgba::from_black_alpha(0.0)
-    }
-}
-
-/// Display the image at a fixed size
-const SIZE: f32 = 128.0;
 
 fn main() {
     let mut args = env::args();
@@ -140,14 +31,14 @@ fn main() {
         for file in args {
             let content = fs::read(&file).unwrap();
             if GifDecoder::new(&content[..]).is_ok() {
-                new_gif = Some((file, content));
+                new_gif = Some(content);
             } else {
                 let sound = StaticSoundData::from_cursor(
                     Cursor::new(content.clone()),
                     StaticSoundSettings::default(),
                 );
                 if sound.is_ok() {
-                    new_audio = Some((file, content));
+                    new_audio = Some(content);
                 } else {
                     // Can't interpret that first file as a gif or an audio file
                     panic!("Invalid file : {file}");
@@ -155,46 +46,154 @@ fn main() {
             }
         }
 
-        staticdata::rewrite_exe(&exe, new_gif, new_audio);
+        staticdata::mutself(&exe, new_gif.as_deref(), new_audio.as_deref());
         return;
     }
 
-    let decoder = GifDecoder::new(staticdata::get_gif_data()).unwrap();
-    let frames = decoder
+    let decoder = GifDecoder::new(Cursor::new(&*staticdata::GIF)).unwrap();
+    let (width, height) = decoder.dimensions();
+    let frames: Vec<_> = decoder
         .into_frames()
-        .collect_frames()
-        .expect("Can't decode frames");
+        .map(|f| {
+            let mut f = f.unwrap();
+            for p in f.buffer_mut().chunks_exact_mut(4) {
+                p.copy_from_slice(&[p[2], p[1], p[0], p[3]]);
+            }
+            f
+        })
+        .collect();
 
-    eframe::run_native(
-        "Vibin",
-        eframe::NativeOptions {
-            always_on_top: true,
-            decorated: false,
-            initial_window_size: Some(Vec2::new(SIZE, SIZE)),
-            initial_window_pos: Some(Pos2::new(100.0, 100.0)),
-            vsync: true,
-            resizable: false,
-            transparent: true,
-            ..Default::default()
-        },
-        Box::new(move |cc| {
-            let images = frames
-                .iter()
-                .enumerate()
-                .map(|(i, f)| {
-                    let handle = cc.egui_ctx.load_texture(
-                        format!("gif_frame_{i}"),
-                        ColorImage::from_rgba_unmultiplied(
-                            [f.buffer().width() as _, f.buffer().height() as _],
-                            f.buffer(),
-                        ),
-                        TextureOptions::LINEAR,
-                    );
-                    let (num, den) = f.delay().numer_denom_ms();
-                    (handle, (num as f32 * 1000.0 / den as f32).round() as u32)
-                })
-                .collect();
-            Box::new(Vibin::new(cc, images))
-        }),
-    );
+    let event_loop = EventLoop::new();
+    let mut builder = WindowBuilder::new()
+        .with_decorations(false)
+        .with_title("Vibin")
+        .with_transparent(true)
+        // .with_taskbar_icon(Some(
+        //     Icon::from_rgba(frames[0].buffer().to_vec(), width, height).unwrap(),
+        // ))
+        .with_window_level(WindowLevel::AlwaysOnTop)
+        .with_resizable(false)
+        .with_inner_size(LogicalSize::new(width, height));
+
+    #[cfg(windows)]
+    {
+        use winit::platform::windows::WindowBuilderExtWindows;
+        builder = builder
+            .with_skip_taskbar(false)
+            .with_undecorated_shadow(false);
+    }
+
+    let window = builder.build(&event_loop).unwrap();
+    let mut gctx = unsafe { GraphicsContext::new(&window, &window).unwrap() };
+
+    let mut manager = AudioManager::<DefaultBackend>::new(AudioManagerSettings::default())
+        .expect("Can't initialize audio context");
+
+    let settings = StaticSoundSettings::new()
+        .volume(0.8)
+        .loop_behavior(LoopBehavior {
+            start_position: 0.0,
+        });
+
+    let audio = StaticSoundData::from_cursor(Cursor::new(&*staticdata::AUDIO), settings)
+        .expect("Can't decode bundled sound file");
+
+    manager.play(audio).expect("Can't play sound");
+    let mut volume = 0.2;
+    manager
+        .main_track()
+        .set_volume(volume, Tween::default())
+        .unwrap();
+
+    let mut last_redraw = Instant::now();
+    let mut i = 0;
+
+    let mut drag = false;
+    let mut prev_pos = window.outer_position().unwrap();
+    let (numer, denom) = frames[i].delay().numer_denom_ms();
+    let mut delay = Duration::from_secs_f32(numer as f32 / denom as f32 / 1000.0);
+
+    event_loop.run(move |e, _, control_flow| {
+        match e {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CursorMoved { position, .. } => {
+                    if drag {
+                        let wpos = window.outer_position().unwrap();
+                        window.set_outer_position(PhysicalPosition {
+                            x: wpos.x + position.x as i32 - prev_pos.x,
+                            y: wpos.y + position.y as i32 - prev_pos.y,
+                        });
+                    } else {
+                        prev_pos = position.cast::<i32>();
+                    }
+                }
+                WindowEvent::MouseInput { button, state, .. } => match button {
+                    MouseButton::Middle => {
+                        control_flow.set_exit();
+                        return;
+                    }
+                    MouseButton::Right => {
+                        if state == ElementState::Pressed {
+                            match manager.state() {
+                                MainPlaybackState::Paused | MainPlaybackState::Pausing => manager
+                                    .resume(Tween {
+                                        start_time: StartTime::Immediate,
+                                        duration: Duration::from_millis(350),
+                                        easing: Easing::InPowf(1.0),
+                                    })
+                                    .expect("Can't resume audio"),
+                                MainPlaybackState::Playing => manager
+                                    .pause(Tween {
+                                        start_time: StartTime::Immediate,
+                                        duration: Duration::from_millis(350),
+                                        easing: Easing::OutPowf(1.0),
+                                    })
+                                    .expect("Can't pause audio"),
+                            }
+                        }
+                    }
+                    MouseButton::Left => match state {
+                        ElementState::Pressed => {
+                            drag = true;
+                        }
+                        ElementState::Released => {
+                            drag = false;
+                        }
+                    },
+                    _ => {}
+                },
+                WindowEvent::MouseWheel { delta, .. } => {
+                    let amount = match delta {
+                        MouseScrollDelta::LineDelta(_, y) => y as f64,
+                        MouseScrollDelta::PixelDelta(PhysicalPosition { y, .. }) => y,
+                    };
+                    volume = (volume + amount / 60.0).clamp(0.0, 1.0);
+                    manager
+                        .main_track()
+                        .set_volume(volume, Tween::default())
+                        .expect("Can't set volume");
+                }
+                _ => {}
+            },
+            Event::RedrawRequested(_) => {
+                // No way I'm making a copy on each redraw
+                let slice = frames[i].buffer().as_raw().as_slice();
+                let (ptr, len) = (slice.as_ptr(), slice.len());
+                gctx.set_buffer(
+                    unsafe { slice::from_raw_parts(ptr as *const u32, len / 4) },
+                    width as u16,
+                    height as u16,
+                );
+            }
+            _ => {}
+        }
+        if last_redraw.elapsed() >= delay {
+            i = (i + 1) % frames.len();
+            last_redraw = Instant::now();
+            let (numer, denom) = frames[i].delay().numer_denom_ms();
+            delay = Duration::from_secs_f32(numer as f32 / denom as f32 / 1000.0);
+            control_flow.set_wait_until(last_redraw + delay);
+            window.request_redraw();
+        }
+    });
 }
